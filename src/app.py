a4b8e3b8bfc0ec21dad85ce1a1d352622f52dd83
Rami Piik <rami.piik@gmail.com>
2022-02-23 21:09:41 +0200

    add poetry and flask

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+@app.route("/1")
+def page1():
+    return "Hi there!"
+
+@app.route("/2")
+def page2():
+    return "What's up?"
