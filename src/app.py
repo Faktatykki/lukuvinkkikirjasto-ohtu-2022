bc7fdc0d40febd2c1c3645cf2054db41bfc45530
Rami Piik <rami.piik@gmail.com>
2022-02-24 23:12:55 +0200

    test db URL

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 
