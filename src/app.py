3d5538c3378fad1857509f7f76f9959419312244
Rami Piik <rami.piik@gmail.com>
2022-02-24 23:27:28 +0200

    test db URL

--- src/app.py
+++ src/app.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://xxx' # Has to defined, but will be overwritten by the Heroku config variables.
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL") # Has to defined, but will be overwritten by the Heroku config variables.
 db = SQLAlchemy(app)
 
 
