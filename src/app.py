40f52162500ee5dbe9d97d4ab827dafbe12e9440
Rami Piik <rami.piik@gmail.com>
2022-02-25 17:27:40 +0200

    db config

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL") # Has to defined, but will be overwritten by the Heroku config variables.
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
