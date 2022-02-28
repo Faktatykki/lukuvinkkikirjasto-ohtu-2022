776db4d587cce899102ceedeb2a2b9fa5d61cbb7
Rami Piik <rami.piik@gmail.com>
2022-02-28 20:08:57 +0200

    linting

--- src/data/db.py
+++ src/data/db.py
@@ -1,7 +1,8 @@
-from app import app
 from flask_sqlalchemy import SQLAlchemy
+from app import app
 
 db = SQLAlchemy(app)
+
 
 def get_all_tips():
     '''hakee tietokannasta kaikki vinkit'''
@@ -11,15 +12,15 @@ def get_all_tips():
 
     return result
 
+
 def add_tip(title: str, url: str) -> bool:
     '''yrittää lisätä tietokantaan uuden vinkin. Jos onnistuu = palauttaa True, jos ei = False'''
     try:
         sql = "INSERT INTO tips (title, url) VALUES (:title, :url)"
-        db.session.execute(sql, {"title":title, "url":url})
+        db.session.execute(sql, {"title": title, "url": url})
         db.session.commit()
-    except Exception as e:
+    except Exception as exception:
+        print(exception)
         return False
 
     return True
-
-    
--- src/ui/controller.py
+++ src/ui/controller.py
@@ -2,7 +2,7 @@ from flask import redirect, request, render_template
 
 from app import app
 
-import logic.appLogic as logic
+import logic.app_logic as logic
 
 
 @app.route("/mainpage")
@@ -10,10 +10,12 @@ def browse_tips():
     '''Näyttää pääsivun jossa näkyy tietokannasta löytyvät vinkit ja lomake jolla lisätä uusi'''
     tips = logic.get_all_tips()
 
-    return render_template("main_page.html", tips = tips)
+    return render_template("main_page.html", tips=tips)
 
-#if add_tip returns false, maybe some warning?
-@app.route("/add", methods = ["POST"])
+# if add_tip returns false, maybe some warning?
+
+
+@app.route("/add", methods=["POST"])
 def add_tip():
     """Tekee post-pyynnön käyttäen lomakkeesta saatuja parametreja,
         eli lähettää uuden vinkin kutsuen logic-layerin add_tip-funktiota, joka palauttaa boolean arvon.
@@ -21,13 +23,11 @@ def add_tip():
      """
     title = request.form["title"]
     url = request.form["url"]
-    
+
     success = logic.add_tip(title, url)
 
     if success:
         return redirect("/mainpage")
-    else:
-        print("Something went wrong")
 
+    print("Something went wrong")
     return redirect("/mainpage")
-
