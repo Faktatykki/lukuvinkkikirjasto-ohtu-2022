c26c87cec0d6c3a2a8027f14d9e4c8042e76bf14
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-03-01 02:45:51 +0200

    tryhello

--- src/app.py
+++ src/app.py
@@ -5,15 +5,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Has to defined, but will be overwritten by the Heroku config variables.
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-#important that this import is after variable declaration
-import ui.controller
-
-
 @app.route("/")
 def index():
-    '''Ohjaa pääsivulle'''
-    return redirect("/mainpage")
+    return "hello world"
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+# #important that this import is after variable declaration
+# import ui.controller
+
+
+# @app.route("/")
+# def index():
+#     '''Ohjaa pääsivulle'''
+#     return redirect("/mainpage")
