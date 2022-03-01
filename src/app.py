f4b6a6172b361f24bc59c633041687238a9ac866
ElectricShakuhachi <haavistoaaro@gmail.com>
2022-03-01 02:52:56 +0200

    revert

--- src/app.py
+++ src/app.py
@@ -5,29 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Has to defined, but will be overwritten by the Heroku config variables.
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-class DingDong:
-    def __init__(self):
-        self.diipadaapa = "10"
+#important that this import is after variable declaration
+import ui.controller
 
-    def __str__(self):
-        return self.diipadaapa
-
-thing = DingDong()
 
 @app.route("/")
 def index():
-    return str(thing)
-
-# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-
-# #important that this import is after variable declaration
-# import ui.controller
-
-
-# @app.route("/")
-# def index():
-#     '''Ohjaa pääsivulle'''
-#     return redirect("/mainpage")
+    '''Ohjaa pääsivulle'''
+    return redirect("/mainpage")
