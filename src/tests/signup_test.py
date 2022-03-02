d71a120d1421368b876453121fa2df6c447b9f71
Rami Piik <rami.piik@gmail.com>
2022-03-02 21:52:24 +0200

    add unit test for user class

--- src/tests/signup_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from entities import user
-
-class TestSignUp(unittest.TestCase):
-    def setUp(self):
-        print("Set up goes here")
-
-    def test_hello_world(self):
-        self.assertEqual("Hello world", "Hello world")
