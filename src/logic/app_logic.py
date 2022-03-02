704b99a75c508d533abf4ca8fa76596f0d10f9d9
Sampo Kääriäinen <sampo.kaariainen@gmail.com>
2022-03-02 15:20:59 +0200

    Checks for empty parameters when insterting tip in app_logic

--- src/logic/app_logic.py
+++ src/logic/app_logic.py
@@ -4,11 +4,19 @@ from data import db
 
 def get_all_tips() -> list:
     '''kutsuu data-layeria ja saa vastaukseksi tietokannasta löytyvät vinkit'''
+    
     tips = db.get_all_tips()
 
     return tips
 
 
 def add_tip(title: str, url: str) -> bool:
-    '''passaa eteenpäin käyttöliittymästä saadut parametrit uuden vinkin lisäämiseksi data-layerille'''
+    """
+        passaa eteenpäin käyttöliittymästä saadut parametrit uuden vinkin lisäämiseksi data-layerille.
+        Tarkistetaan myös että parametrit eivät ole tyhjiä (myös välilyöntien varalta)
+    """
+
+    if not title.strip() or not url.strip():
+        return False
+
     return db.add_tip(title, url)
