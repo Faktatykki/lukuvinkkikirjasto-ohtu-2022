34753d9a816de97956c4a01334894d6a16f63319
Jere Nuora <jere.nuora@gmail.com>
2022-03-05 01:48:13 +0200

    remove print-debugging

--- src/logic/user_logic.py
+++ src/logic/user_logic.py
@@ -9,7 +9,6 @@ def signup(username:str, password:str, admin=False):
     hashed_password = generate_password_hash(password)
     res=db.add_user(username, hashed_password, admin)
     try:
-        print('onko resissä user id', res)
         if "user_id" in res:
             # print("Tietokanta palauttaa1:", res["user_id"], res["username"], res["admin"])
             return signin(username, password)
@@ -20,12 +19,9 @@ def signup(username:str, password:str, admin=False):
 def signin(username:str, password:str):
     '''Palauttaa User-olion, jos kirjautuminen onnistuu'''
     user_in_database = db.get_user(username)
-    print('user_in_database', user_in_database)
     if user_in_database:
         passwords_match = check_password_hash(user_in_database["password"], password)
-        print('passwords_match',passwords_match)
         if passwords_match:
             new_user = user.User(user_in_database["user_id"], user_in_database["username"])
-            print('nyy user',new_user)
             return new_user
     return False
